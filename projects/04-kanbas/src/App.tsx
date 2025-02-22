0e1f947bc1f42f270e854a620eb5b5ac91fbcd26
Alejandro Castro <150183777+r-alejandro-castro@users.noreply.github.com>
2025-02-22 01:03:59 -0400

    finish 04-kanbas

--- projects/04-kanbas/src/App.tsx
+++ projects/04-kanbas/src/App.tsx
@@ -1,8 +1,11 @@
+import  Kanban  from './components/kanban/Kanban';
+
 export default function App() {
   return (
     <div className="p-0 m-0 box-border">
         <div className='p-[50px]'>
-            <h1 className='mb-[20px] font-bold text-4xl'>Kanban UI</h1>
+            <h1 className='mb-[20px] font-bold text-4xl text-night'>Kanban UI</h1>
+            <Kanban/>
         </div>
     </div>
   )
