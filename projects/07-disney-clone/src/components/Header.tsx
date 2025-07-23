2a505fcaa405a254019351db9c271c2e6a3cedf5
Rodolfo Castro <soyracm@gmail.com>
2025-07-23 16:00:58 -0400

    responsive navbar

--- projects/07-disney-clone/src/components/Header.tsx
+++ projects/07-disney-clone/src/components/Header.tsx
@@ -1,18 +1,89 @@
+import { useState } from 'react';
 import logo from '../assets/logo.svg';
-import { HeaderItem } from './ui/HeaderItem';
 import user from "../assets/user.png"
+import { 
+  HiHome,
+  HiMagnifyingGlass,
+  HiStar,
+  HiPlayCircle,
+  HiTv
+} from 'react-icons/hi2';
+import {
+  HiDotsVertical,
+  HiPlus,
+} from "react-icons/hi"
+import { HeaderItem } from './ui/HeaderItem';
 
 
 export const Header = () => {
 
+
+  const [toggle,setToggle] = useState(false);
+
+  const menu=[
+      {
+      name:'HOME',
+      icon:HiHome
+      },
+      {
+      name:'SEARCH',
+      icon:HiMagnifyingGlass
+      },
+      {
+      name:"WATCH LIST",
+      icon:HiPlus
+      },
+      {
+      name:'ORIGINALS',
+      icon:HiStar
+      },
+      {
+      name:'MOVIES',
+      icon:HiPlayCircle
+      },
+      {
+      name:'SERIES',
+      icon:HiTv
+      }
+  ];
+  
+
   return (
+    
     <>
-        <div className='flex items-center justify-between p-5'>  
-          <div className="flex gap-8 items-center">
-            <img src={logo} alt="logo" className='w-[80px] md:w-[105px] object-cover p-2'/>
-            <HeaderItem/>
+        <div className='flex items-center justify-between p-5'>
+          <div className='flex items-center gap-8'>
+            {/* Logo */}
+            <img src={logo} alt="logo" className='w-[80px] lg:w-[105px] object-cover p-2'/>
+
+            {/* Barra */}
+            <div className="hidden lg:flex gap-8">
+              {menu.map((item) => (
+                <HeaderItem key={item.name} name={item.name} Icon={item.icon}/>
+              ))}
+            </div>
+
+            {/* Barra Responsive */}
+            <div className="flex lg:hidden gap-5 items-center ">
+              {menu.map((item,index) => index < 3 && (
+                <HeaderItem key={item.name} name={''} Icon={item.icon}/>
+              ))}
+              <div className="relative">
+                <div onClick={() => setToggle(!toggle)}>
+                  <HeaderItem name={''} Icon={HiDotsVertical}/>
+                </div>
+                {toggle && (
+                  <div className='absolute mt-3 bg-[#121212] border-[1px] border-gray-700 p-3 px-5 py-4 right-0 rounded-md flex flex-col gap-3'>
+                    {menu.map((item,index) => index > 2 && (
+                      <HeaderItem key={item.name} name={item.name} Icon={item.icon}/>
+                    ))}
+                  </div>
+                )}
+              </div>
+            </div>
+
           </div>
-          <img src={user} alt="user" className='w-[40px] rounded-full'/>
+          <img src={user} alt="user" className='w-[40px] rounded-full '/>
         </div>
     </>
   )
--- projects/07-disney-clone/src/components/ui/HeaderItem.tsx
+++ projects/07-disney-clone/src/components/ui/HeaderItem.tsx
@@ -1,52 +1,19 @@
-import { 
-  HiHome,
-  HiMagnifyingGlass,
-  HiStar,
-  HiPlayCircle,
-  HiTv
-} from 'react-icons/hi2';
-import {
-  HiPlus, 
-//   HiDotsVertical
-} from "react-icons/hi"
+import type { ElementType } from 'react';
 
-export const HeaderItem = () => {
-    
-    const menu=[
-        {
-        name:'HOME',
-        icon:HiHome
-        },
-        {
-        name:'SEARCH',
-        icon:HiMagnifyingGlass
-        },
-        {
-        name:"WATCH LIST",
-        icon:HiPlus
-        },
-        {
-        name:'ORIGINALS',
-        icon:HiStar
-        },
-        {
-        name:'MOVIES',
-        icon:HiPlayCircle
-        },
-        {
-        name:'SERIES',
-        icon:HiTv
-        }
-    ];
+interface HeaderItemProsp {
+    name: string;
+    Icon: ElementType;
+}
+
+export const HeaderItem = ({name, Icon}: HeaderItemProsp) => {
 
     return (
-    <div className='flex gap-8'>
-        {menu.map((item, idx) => (
-            <div key={idx} className='flex text-white items-center gap-3 text-[15px] font-semibold cursor-pointer hover:underline underline-offset-8'>
-                <item.icon size={20}/>
-                <span>{item.name}</span>
-            </div>
-        ))}
-    </div>
+        <div 
+            className='flex text-white items-center gap-3 text-[15px] font-semibold cursor-pointer hover:underline underline-offset-8'
+        >
+            <Icon size={20}/>
+            <span>{name}</span>
+        </div>
+
   )
 }
