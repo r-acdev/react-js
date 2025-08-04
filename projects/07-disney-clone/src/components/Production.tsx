181779891547e17151b26471781e18c9b78d5663
Rodolfo Castro <soyracm@gmail.com>
2025-08-04 14:13:43 -0400

    changes

--- projects/07-disney-clone/src/components/Production.tsx
+++ projects/07-disney-clone/src/components/Production.tsx
@@ -53,7 +53,7 @@ export const Production = () => {
   ]
 
   return (
-    <div className='flex gap-2 md:gap-5 p-2 px-5 md:px-16'>
+    <div className='flex gap-2 md:gap-5 p-2 px-5 md:px-16 items-center justify-center'>
         {productionList.map((item)=>(
             <div key={item.id} className='border-[2px] border-gray-600 rounded-lg hover:scale-110 transition-all duration-300 ease-in-out cursor-pointer relative shadow-xl shadow-gray-900'>
                 <video src={item.video} autoPlay loop playsInline muted className='absolute z-0 top-0 rounded-md opacity-0 hover:opacity-50'/> 
