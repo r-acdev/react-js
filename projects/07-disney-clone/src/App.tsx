e0f81b50253deb19a6c1934b1d126021dd403a71
Rodolfo Castro <soyracm@gmail.com>
2025-08-04 14:18:43 -0400

    changes

--- projects/07-disney-clone/src/App.tsx
+++ projects/07-disney-clone/src/App.tsx
@@ -2,7 +2,6 @@ import { Header } from './components/Header'
 import { Slider } from './components/Slider'
 import { Production } from './components/Production'
 import { GenereMovie } from './components/GenereMovie'
-import './App.css'
 
 function App() {
 
